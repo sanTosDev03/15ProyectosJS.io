791e44a5670be8399eff40419c0ea639f7a41bda
SantosDev <santosdevtech@gmail.com>
2023-11-27 07:50:51 -0300

    Color Flipper Finished

---
+++ ColorFlipper/app.js
@@ -0,0 +1,17 @@
+const colors = ["green", "red", "rgba(133,122,200", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+
+// Change de background color when i'm click on the buttom 
+btn.addEventListener('click', e => {
+    // get random number between 0 - 3;
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
